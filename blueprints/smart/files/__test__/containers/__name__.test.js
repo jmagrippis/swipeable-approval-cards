5cd8e9d23e71c2a0481cf8a84e75e64429a1d46e
Johnny Magrippis <j@magrippis.com>
2016-05-22 20:20:22 +0100

    Changes the paths for the smart components

--- blueprints/smart/files/__test__/containers/__name__.test.js
+++ blueprints/smart/files/__test__/containers/__name__.test.js
@@ -2,10 +2,10 @@ import React from 'react'
 import { expect } from 'chai'
 import { shallow } from 'enzyme'
 
-import <%= pascalEntityName %> from '../../containers/<%= pascalEntityName %>'
+import <%= pascalEntityName %> from '../../src/containers/<%= pascalEntityName %>'
 
 describe('(Container) <%= pascalEntityName %>', () => {
   it('should exist', () => {
-    expect(shallow(<%= pascalEntityName %>).contains(<div></div>)).to.equal(true)
+    expect(shallow(<<%= pascalEntityName %> />).contains(<div></div>)).to.equal(true)
   })
 })
